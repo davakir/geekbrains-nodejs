1d5c80faf2a0492cbbdc59142636ef1065d9b4a4
Darya Kiryanova <bella21abyss@gmail.com>
2017-04-12 05:51:45 +0700

    Init commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const http = require("http");
+const ansi = require("ansi");
+
+var server = http.createServer(function(req, res) {
+    res.writeHead(200);
+    res.end("Hello Http\n");
+});
+
+server.listen(8080);
