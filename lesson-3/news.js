320ffb58a195314cb206313a3f16bc11d700710b
Darya Kiryanova <bella21abyss@gmail.com>
2017-04-24 22:02:26 +0300

    Доработка второго урока

--- lesson-3/news.js
+++ lesson-3/news.js
@@ -1,26 +1,20 @@
 const http = require('http');
 const request = require('request');
 const parseString = require('xml2js').parseString;
-const ansi = require('ansi');
-const cursor = ansi(process.stdout);
 
 const server = http.createServer(function (req, res) {
     res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
     request('https://rakh.im/feed.xml', (error, response, body) => {
         if (!error && response.statusCode === 200) {
             parseString(body, (err, result) => {
+                res.write('<h1>Название потока: ' + result.rss.channel[0].title + '</h1><br><br>');
 
-                res.write('Название потока: ' + result.rss.channel[0].title + "<br>");
-                res.write('Заголовок: ' + result.rss.channel[0].item[0].title + "<br>");
-                // res.write(result.rss.channel[0].item[0].description);
-                res.write('Дата публикации: ' + result.rss.channel[0].item[0].pubDate + "<br>");
-
-                /*                cursor.red()
-                 .write('Название потока: ' + res.rss.channel[0].title + '\n')
-                 .green().write(res.rss.channel[0].item[0].title + '\n')
-                 .blue().write(res.rss.channel[0].item[0].description + '\n')
-                 .grey().write(res.rss.channel[0].item[0].pubDate + '\n')
-                 .reset();*/
+                for (let i = 0; i < result.rss.channel[0].item.length; ++i) {
+                    let item = result.rss.channel[0].item[i];
+                    res.write('<strong>Заголовок: ' + item.title + '</strong><br>');
+                    res.write('' + item.description + '<br>');
+                    res.write('<i>Дата публикации: ' + item.pubDate + '</i><br><br>');
+                }
             });
         }
         else {
